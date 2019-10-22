7ec6e7ba277d8ef35774c048c19cda4c738eb91e
DAVID GUENTHER <davidcarlguenther@gmail.com>
2019-10-21 19:38:23 -0500

    added top level App component

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+
+        }
+
+        // bind functions here
+    }
+
+    render() {
+        return (
+            <div>
+                This will be the App!
+            </div>
+        )
+    }
+}
+
+export default App;
