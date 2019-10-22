ca53479621766e023ed48e966f0bc43ebfacd720
DAVID GUENTHER <davidcarlguenther@gmail.com>
2019-10-22 16:37:49 -0500

    shopping cart added to navBar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from '../../assets/images/BMH.png';
+import cart from '../../assets/images/shopping-cart.png';
 
 const Navigation = (props) => {
     return (
         <div className="navigation">
-            <img src={logo} />
-            This is the Navigation!
-       </div>
+            <img className="nav-logo" src={logo} />
+            <div className="cart">
+                <img className="cart-image" src={cart} />
+            </div>
+        </div>
     )
 }
 
