b02e306cbe68e638bd36e01bef35cb4a60fad621
DAVID GUENTHER <davidcarlguenther@gmail.com>
2019-10-30 16:39:18 -0500

    added images to cards

--- src/components/ImageDisplay.js
+++ src/components/ImageDisplay.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 const ImageDisplay = (props) => {
-    console.log('props: ', props);
+    console.log('prop', props)
     return (
         <div>
-            <img src={props.picture.image} />
+            <Card>
+                <Card.Img className="solo-pic" variant="top" src={props.picture.image} />
+                <Card.Body>
+                    <Card.Title className="card-text">Image Title</Card.Title>
+                    <Card.Text className="card-text">
+                        <div>Price: ${props.picture.price}</div>
+                        <div>Rating: {props.picture.rating}</div>
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+            </Card>
+            {/* <img className="solo-pic" src={props.picture.image} />
             <div>
                 ${props.picture.price}
                 {props.picture.rating}
-            </div>
+            </div> */}
         </div >
     )
 }
