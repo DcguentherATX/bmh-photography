edbdd7e1272af3160208ddf29a0fb1a36e59935f
DAVID GUENTHER <davidcarlguenther@gmail.com>
2019-10-31 16:40:58 -0500

    added social media icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import instagram from '../../assets/images/instagramLogoBlack.jpg';
+import linkedIn from '../../assets/images/linkedInLogoWhite.png';
+
 const Footer = (props) => {
     return (
         <div className="footer-container">
-            BMH Photography
+            <div className="social-media">
+                <a href="" target="_blank"><img className="social-logo" src={linkedIn} alt="linkedIn logo" /></a>
+                <a href="" target="_blank"><img className="social-logo instagram-logo" src={instagram} alt="instagram logo" /></a>
+            </div>
         </div>
     )
 }
