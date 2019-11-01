b55bb148dc5e4fc4529b35f866eefccfa8f536f5
DAVID GUENTHER <davidcarlguenther@gmail.com>
2019-10-31 21:37:04 -0500

    styled the navbar with dropdown functionality

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
 import logo from '../../assets/images/BMH.png';
@@ -7,7 +8,40 @@ import cart from '../../assets/images/shopping-cart.png';
 const Navigation = (props) => {
     return (
         <div className="navigation">
-            <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+            <Navbar className="navbar" sticky="top">
+                <Navbar.Brand href="#top">
+                    {/* <img width="150" height="70" className="logo" alt="BMH Photography Logo" /> */}
+                </Navbar.Brand>
+                <div className="nav-dropdown">
+                    <DropdownButton className="overall-dropdown"
+                        alignRight
+                        title={
+                            <img className="menu" src={logo} className="nav-logo"></img>
+                        }
+                        id="dropdown-menu-align-right"
+                    >
+                        <Dropdown.Item className="drop" eventKey="1">Beach</Dropdown.Item>
+                        <Dropdown.Item className="drop" eventKey="2">Architecture</Dropdown.Item>
+                        <Dropdown.Item className="drop" eventKey="3">Door</Dropdown.Item>
+                    </DropdownButton>
+                </div>
+                <div className="cart">
+                    <img className="cart-image" src={cart} />
+                </div>
+                {/* <img className="menu" src={menu}></img> */}
+            </Navbar>
+            {/* <Navbar bg="dark">
+                <Navbar.Brand href="#home">
+                    <img
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="React Bootstrap logo"
+                    />
+                </Navbar.Brand>
+            </Navbar>
+            <DropdownButton id="dropdown-basic-button" title="Dropdown button" src={logo}>
                 <Dropdown.Item href="#/action-1">Beach</Dropdown.Item>
                 <Dropdown.Item href="#/action-2">Architecture</Dropdown.Item>
                 <Dropdown.Item href="#/action-3">Doorways</Dropdown.Item>
@@ -15,8 +49,8 @@ const Navigation = (props) => {
             <img className="nav-logo" src={logo} />
             <div className="cart">
                 <img className="cart-image" src={cart} />
-            </div>
-        </div>
+            </div> */}
+        </div >
     )
 }
 
